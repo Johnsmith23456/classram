7b3901e04887edfcd4fd84f1f465b74fee3a0d45
johnsmith23456 <johnsmith2345678190@gmail.com>
2025-04-03 12:33:58 -0400

    A

--- Custom/emulator.js
+++ Custom/emulator.js
@@ -591,7 +591,7 @@ class EmulatorJS {
             if (res === -1) {
                 console.log("File not found, attemping to fetch from emulatorjs cdn.");
                 console.error("**THIS METHOD IS A FAILSAFE, AND NOT OFFICIALLY SUPPORTED. USE AT YOUR OWN RISK**");
-                res = await this.downloadFile(`https://cdn.emulatorjs.org/${this.ejs_version}/data/${corePath}`, (progress) => {
+                res = await this.downloadFile(`https://cdn.jsdelivr.net/gh/Johnsmith23456/pleaseworkaaaaaa/${this.ejs_version}/data/${corePath}`, (progress) => {
                     this.textElem.innerText = this.localization("Download Game Core") + progress;
                 }, true, {responseType: "arraybuffer", method: "GET"});
                 if (res === -1) {
